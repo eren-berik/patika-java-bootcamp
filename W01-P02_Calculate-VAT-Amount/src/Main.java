7aeece57926dd8e0573928b4a9bf8ead7cbb938e
eren-berik <erenberik@outlook.com>
2024-02-21 13:18:52 +0300

    Remove unnecessary code block

--- W01-P02_Calculate-VAT-Amount/src/Main.java
+++ W01-P02_Calculate-VAT-Amount/src/Main.java
@@ -1,9 +1,6 @@
 import java.util.Scanner;
 
 public class Main {
-    public Main() {
-    }
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double itemPrice, vatAmount, totalAmount;
