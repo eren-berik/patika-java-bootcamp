46451e9427422472f58e6d9cf1bd98e38ab82111
eren-berik <erenberik@outlook.com>
2024-02-21 12:45:10 +0300

    [Practice]Calculate VAT and total amount

---
+++ W01-P02_Calculate-VAT-Amount/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double itemPrice, vatAmount, totalAmount;
+
+        System.out.print("Enter the price of the item: ");
+        itemPrice = input.nextDouble();
+        vatAmount = calculateVatAmount(itemPrice);
+        totalAmount = vatAmount + itemPrice;
+
+        System.out.println("Price without VAT: " + String.format("%.2f", itemPrice));
+        System.out.println("Price with VAT: " + String.format("%.2f", totalAmount));
+        System.out.println("VAT amount: " + String.format("%.2f", vatAmount));
+    }
+
+    private static double decideVatPercentage(double itemPrice) {
+        double vatThreshold = 1000.0;
+        double vatPercentage;
+        if (itemPrice > vatThreshold) {
+            vatPercentage = 0.08;
+        } else {
+            vatPercentage = 0.18;
+        }
+
+        return vatPercentage;
+    }
+
+    private static double calculateVatAmount(double itemPrice) {
+        return decideVatPercentage(itemPrice) * itemPrice;
+    }
+}
