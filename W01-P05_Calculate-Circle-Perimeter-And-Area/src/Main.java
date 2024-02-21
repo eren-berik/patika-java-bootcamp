ef3f3de716dbc2ba4d1fc21c6834212db06497cf
eren-berik <erenberik@outlook.com>
2024-02-21 14:02:02 +0300

    Refactor main to increase readibility of console output

--- W01-P05_Calculate-Circle-Perimeter-And-Area/src/Main.java
+++ W01-P05_Calculate-Circle-Perimeter-And-Area/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         radius = input.nextDouble();
 
         System.out.println("----- PI value taken as " + Math.PI + " -----");
-        System.out.println("Results are up to 5 decimal precision\n");
+        System.out.println("----- Results are up to 5 decimal precision -----\n");
         System.out.println("Perimeter of the circle: " + String.format("%.5f", calculateCirclePerimeter(radius)));
         System.out.println("Area of the circle: " + String.format("%.5f", calculateCircleArea(radius)));
     }
