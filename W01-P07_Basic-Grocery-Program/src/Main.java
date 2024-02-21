8025bbd85ecc2bcb3fbc73800ba48a4941df2acf
eren-berik <erenberik@outlook.com>
2024-02-21 22:35:46 +0300

    Define constants at the class level

--- W01-P07_Basic-Grocery-Program/src/Main.java
+++ W01-P07_Basic-Grocery-Program/src/Main.java
@@ -1,8 +1,22 @@
 import java.util.Scanner;
 
+/**
+ * This program calculates the total price of groceries based on the user input for the kilograms of various items.
+ * The user is prompted to enter the weight of pears, apples, tomatoes, bananas, and eggplants, and the program
+ * computes the total cost by multiplying the weights with predefined prices per kilogram.
+ * The constants for prices per kilogram are defined at the class level.
+ */
+
 public class Main {
+    // Define constant variables
+    private static final double PEAR_KG_PRICE = 2.14;
+    private static final double APPLE_KG_PRICE = 3.67;
+    private static final double TOMATO_KG_PRICE = 1.11;
+    private static final double BANANA_KG_PRICE = 0.95;
+    private static final double EGGPLANT_KG_PRICE = 5.00;
+
     public static void main(String[] args) {
-        double pearKg, appleKg,tomatoKg, bananaKg, eggplantKg;
+        double pearKg, appleKg, tomatoKg, bananaKg, eggplantKg;
         Scanner input = new Scanner(System.in);
 
         System.out.print("How many kilograms of pears?: ");
@@ -26,12 +40,6 @@ public class Main {
 
     private static double calculateTotalPriceOfGroceries(double pearKg, double appleKg, double tomatoKg,
                                                          double bananaKg, double eggplantKg) {
-        final double PEAR_KG_PRICE = 2.14;
-        final double APPLE_KG_PRICE = 3.67;
-        final double TOMATO_KG_PRICE = 1.11;
-        final double BANANA_KG_PRICE = 0.95;
-        final double EGGPLANT_KG_PRICE = 5.00;
-
         return (pearKg * PEAR_KG_PRICE) + (appleKg * APPLE_KG_PRICE) + (tomatoKg * TOMATO_KG_PRICE) +
                 (bananaKg * BANANA_KG_PRICE) + (eggplantKg * EGGPLANT_KG_PRICE);
     }
