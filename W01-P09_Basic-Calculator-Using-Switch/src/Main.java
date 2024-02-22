1cf9905100858f9c16f20304037aa7a50f98eea4
Eren Berik <erenberik@outlook.com>
2024-02-22 11:29:43 +0300

    [Practice]Basic calculator with switch statement

---
+++ W01-P09_Basic-Calculator-Using-Switch/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double num1, num2;
+        int select;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter the first number: ");
+        num1 = input.nextDouble();
+
+        System.out.print("Please enter the second number: ");
+        num2 = input.nextDouble();
+
+        System.out.println("1 - Addition\n2 - Substraction\n3 - Multiplication\n4 - Division");
+        System.out.println("Enter your choice: ");
+        select = input.nextInt();
+
+        System.out.println(calculator(select, num1, num2));
+    }
+
+    private static String calculator(int select, double num1, double num2) {
+        switch(select) {
+            case 1: return ("Summation: " + (num1 + num2));
+            case 2: return ("Substraction: " + (num1 - num2));
+            case 3: return ("Multiplication: " + (num1 * num2));
+            case 4:
+                if (num2 == 0) {
+                    return ("Division by zero is not allowed!");
+                } else {
+                    return ("Division: " + (num1 / num2));
+                }
+            default: return ("Something went wrong!");
+        }
+    }
+}
