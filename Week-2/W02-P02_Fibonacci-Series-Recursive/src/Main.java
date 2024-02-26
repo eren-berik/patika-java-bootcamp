c8379cd998e8c3cd1b49b71d7170da5bc613e6ff
Eren Berik <erenberik@outlook.com>
2024-02-26 14:07:56 +0300

    [Practice]Fibonacci series with recursive approach

--- Week-2/W02-P02_Fibonacci-Series-Recursive/src/Main.java
+++ Week-2/W02-P02_Fibonacci-Series-Recursive/src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
+/*
+    This Java program takes an input number from the user and prints the Fibonacci series up to that number.
+    It first prompts the user to enter a number. Then, it iterates from 1 to the input number and calls the
+    fibonacciSeries method for each iteration to calculate and print the corresponding Fibonacci number.
+    The fibonacciSeries method recursively computes the nth Fibonacci number. Finally, the program prints
+    the Fibonacci series up to the entered number.
+ */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int num;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        num = sc.nextInt();
+
+        System.out.println("Fibonacci series up to " + num + ":");
+        for (int i = 1; i <= num; i++) {
+            System.out.print(fibonacciSeries(i) + " ");
+        }
     }
+
+    public static int fibonacciSeries(int num) {
+
+        if (num == 1 || num == 2) {
+            return 1;
+        }
+
+        return fibonacciSeries(num - 1) + fibonacciSeries(num - 2);
+    }
+
 }
